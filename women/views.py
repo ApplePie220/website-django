f5cf89163c5253a727b411e5e2ea738aba0207b4
CyberKilljoy <darkmilk85@gmail.com>
2023-03-06 12:09:01 +0300

    add forms and addpage and edit logo

--- women/views.py
+++ women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import *
 from .models import *
 
 menu = [{'title': 'Об этом замечательном сайте', 'url_name': 'about'},
@@ -12,10 +13,8 @@ menu = [{'title': 'Об этом замечательном сайте', 'url_na
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0
@@ -28,7 +27,17 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse('Добавить совершенную статью.')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid(): # корректно ли данные заполнены и переданы
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
@@ -39,8 +48,16 @@ def login(request):
     return HttpResponse('Авторизация')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отожраение статьи с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id
+    }
+    return render(request, 'women/post.html', context=context)
 
 
 def pageNotFound(request, exception):
@@ -49,13 +66,11 @@ def pageNotFound(request, exception):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Рубрики',
         'cat_selected': cat_id
